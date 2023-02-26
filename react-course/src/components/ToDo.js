86ff2612e7bb2dc5440d7a976f1bbefa1f4ad78d
munifhasan <munifhasan@dataemb.com>
2023-02-26 19:37:29 +0600

    handling events

--- react-course/src/components/ToDo.js
+++ react-course/src/components/ToDo.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 function ToDo(props) {
+  function deleteHandler() {
+    console.log(props.title)
+  }
+
   return (
     <div className='card'>
       <h2>{props.title}</h2>
       <div className='actions'>
-        <button className='btn'>Delete</button>
+        <button className='btn' onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
     </div>
   )
