de6c8f2bf8ac3c9db44f0aa209f8daa3c7824087
munifhasan <munifhasan@dataemb.com>
2023-02-26 19:35:32 +0600

    reusing components and passing data with props and dynamic contents.

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,7 +1,12 @@
+import ToDo from './components/ToDo'
+
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>My Todos</h1>
+      <ToDo title='Basic React' />
+      <ToDo title='Intermediate React' />
+      <ToDo title='Advance React' />
     </div>
   )
 }
